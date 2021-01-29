3d3120e6b15e9adc05a60c51426a4e5d4b12e2bd
alabo-excel <iamalaboexcel@gmail.com>
2021-01-29 14:17:36 +0100

    spinner, header and download commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h4>MyUnsplash</h4>
       </header>
       <Images />
     </div>
--- src/components/images.js
+++ src/components/images.js
@@ -11,11 +11,13 @@ function Images() {
     .then((data) => {
       setImage(data)
     })
-  })
-const fetchMoreData = () => {
-    fetch(`https://picsum.photos/v2/list?page=${page}&limit=28`)
-    .then((response) => response.json())
-    .then((data) => {
+  },[])
+
+  const fetchMoreData = () => {
+    setPage(page + 1)
+    fetch(`https://picsum.photos/v2/list?page=${page}&limit=8`)
+    .then(response => response.json())
+    .then(data => {
       setImage([...images, ...data])
     })
   }
@@ -25,15 +27,32 @@ const fetchMoreData = () => {
         dataLength={images.length}
         next={fetchMoreData}
         hasMore={true}
-        loader={<h4>Loading...</h4>}
-      ><div className="row">
+        loader={
+          <div className="d-flex justify-content-center mt-5">
+            <div className="spinner-border" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        }
+      >
+      <div className="row">
         {images.map((image, index) => (
-          <div key={index}  className="col-lg-3">
+          <div key={index}  className="col-lg-3 mb-4">
             <img src={image.download_url} alt={index} />
-            <p>{image.author}</p>
+            <div className="d-flex justify-content-between p-3 on-top">
+              <p>{image.author}</p>
+              <a href={image.download_wurl} download={image.author + '.jpg'}>
+              <div className="download p-2 ">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-download" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+              </svg>
+              </div>
+              </a>
+            </div>
           </div>
         ))}
-        </div>
+      </div>
       </InfiniteScroll>
     </div>
   )
